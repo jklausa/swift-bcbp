8d177d6a95776070eeffa4948b226f6982f15e25
Jan Klausa <jan@klausa.pl>
2025-08-26 16:35:48 +0900

    add a padded parser-printer

---
+++ Sources/SwiftBCBP/Utilities/RightPaddedStringParser.swift
@@ -0,0 +1,29 @@
+import Parsing
+
+struct RightPaddedStringParser: ParserPrinter {
+    let length: Int
+
+    var body: some Parser<Substring, String> {
+        Prefix(length)
+            .map { substring in
+                var substring = substring
+
+                while substring.last == " " {
+                    substring.removeLast()
+                }
+
+                return String(substring)
+            }
+
+    }
+
+    func print(_ output: String, into input: inout Substring) throws {
+        let paddingCount = length - output.count
+
+        for _ in 0 ..< paddingCount {
+            input.prepend(" ")
+        }
+
+        input.prepend(contentsOf: output)
+    }
+}
---
+++ Tests/SwiftBCBPTests/RightPaddedStringParser.swift
@@ -0,0 +1,34 @@
+import Testing
+import Parsing
+@testable import SwiftBCBP
+
+@Test
+func rightPaddedStringParserRoundtrippingWithSpaces() throws {
+    let input = "HELLO   "
+    let parser = RightPaddedStringParser(length: 8)
+
+    let parsed = try parser.parse(input)
+    #expect(parsed == "HELLO")
+
+    var buffer = "" as Substring
+    try parser.print(parsed, into: &buffer)
+
+    #expect(buffer == input)
+}
+
+@Test
+func rightPaddedStringParserRoundtrippingWithoutSpaces() throws {
+    let input = "ELLOH"
+    let parser = RightPaddedStringParser(length: 5)
+
+    let parsed = try parser.parse(input)
+    #expect(parsed == "ELLOH")
+
+    var buffer = "" as Substring
+    try parser.print(parsed, into: &buffer)
+
+    #expect(buffer == input)
+}
+
+
+
